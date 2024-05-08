bfbf1c13367ffe7ab7d52947d764886077726648
Celso Vale <celsoj_@hotmail.com>
2024-05-08 10:16:31 -0300

    Add user email/id to URL

--- app/show-params.tsx
+++ app/show-params.tsx
@@ -10,6 +10,8 @@ export default function ShowParams() {
   const minVer = router.get("minVer");
   const vaultid = router.get("vaultid");
   const latestVersion = router.get("latestVersion");
+  const userId = router.get("userId");
+  const userEmail = router.get("userEmail");
 
   return (
     <ul>
@@ -18,6 +20,8 @@ export default function ShowParams() {
       <li>{minVer}</li>
       <li>{vaultid}</li>
       <li>{latestVersion}</li>
+      <li>{userId}</li>
+      <li>{userEmail}</li>
     </ul>
   );
 }
