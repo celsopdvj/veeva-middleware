909ac1b05a5636c495b8a6369b091256a9dcbb2a
Celso Pereira do vale junior <celsoj_@hotmail.com>
2024-07-26 13:37:23 -0300

    Fix consent message

--- pages/consent/success/index.tsx
+++ pages/consent/success/index.tsx
@@ -5,7 +5,7 @@ export default function ConsentSuccess() {
 
   return (
     <div className="w-full text-center font-semibold leading-6 text-orange-500 p-20">
-      <p>Consent was successfuly grant. You can now close this tab.</p>
+      <p>Consent was successfully granted. You can now close this tab.</p>
     </div>
   );
 }
